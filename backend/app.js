4d856d117d594df1ccc0289273e5647bdfb89aea
Kirill Shcherbinko <kirill.sherbinko@gmail.com>
2024-11-17 19:28:44 +0300

    added dependencies, configured connection

---
+++ backend/app.js
@@ -0,0 +1,27 @@
+import cors from "cors"
+import express from "express"
+import mongoose from "mongoose"
+//import authRouter from "./routers/authRouter.js"
+//import pinRouter from "./routers/pinRouter.js"
+
+const DB_URL = "mongodb://root:root_password@mongodb:27017/mydatabase?authSource=admin"
+const PORT = process.env.PORT || 5000
+
+const app = express()
+
+app.use(cors({origin: "http://frontend"})) // Настройка cors
+app.use(express.json()) // Запросы представлены в формате json
+//app.use("/pins", pinRouter)
+//app.use("/auth", authRouter)
+
+// Функция для запуска проекта
+async function startApp() {
+    try {
+        await mongoose.connect(DB_URL, {useUnifiedTopology: true, useNewUrlParser: true})
+        app.listen(PORT, () => console.log("SEVER STARTED ON PORT " + PORT))
+    } catch(e) {
+        console.log(e)
+    }
+}
+
+startApp()
---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit and save to reload.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
